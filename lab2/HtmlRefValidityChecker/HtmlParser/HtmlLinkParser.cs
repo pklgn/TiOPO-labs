d5dc726feda58e677710dc24341d204f7cbf2c74
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-09-28 01:16:03 +0300

    [lab2] add basics

---
+++ lab2/HtmlRefValidityChecker/HtmlParser/HtmlLinkParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HtmlRefValidityChecker.HtmlParser;
+
+class HtmlLinkParser
+{
+				private ISet<string> m_paths;
+				private HtmlWeb m_htmlWeb;
+    private Uri m_uri;
+
+				public HtmlLinkParser(Uri uri)
+				{
+								m_paths = new HashSet<string>();
+								m_htmlWeb = new HtmlWeb();
+        m_uri = uri;
+				}
+
+				public void TraverseAllPageLinks(Uri uri)
+				{
+        HtmlDocument doc = m_htmlWeb.Load(uri);
+								var paths = doc.DocumentNode.Descendants("a")
+												.Select(a => a.GetAttributeValue("href", null))
+												.Where(link => !String.IsNullOrEmpty(link));
+
+        foreach (var path in paths)
+        {
+            Uri? pathCombinedUri = CombineUriWithPath(uri, path);
+            if (pathCombinedUri != null)
+            {
+                AccountPageLink(pathCombinedUri);
+
+                continue;
+            }
+
+            Uri.TryCreate(path, UriKind.RelativeOrAbsolute, out Uri? pathUri);
+            if (pathUri != null)
+            {
+                if (m_uri.Host == uri.Host)
+                {
+                    AccountPageLink(pathUri);
+                }
+            }
+        }
+				}
+
+    public ISet<string> GetAllPageLinks()
+    {
+        TraverseAllPageLinks(m_uri);
+
+        return m_paths;
+    }
+
+    public static Uri? CombineUriWithPath(Uri uri, string path)
+    {
+        Uri? pathUri = new Uri("about:blank");
+        bool pathUriWasCreated = Uri.TryCreate(uri, path, out pathUri);
+
+        if (!pathUriWasCreated || pathUri == null)
+        {
+            return null;
+        }
+
+        return pathUri;
+    }
+
+    public void AccountPageLink(Uri uri)
+    {
+        if (m_paths.Add(uri.LocalPath))
+        {
+            TraverseAllPageLinks(uri);
+        }
+    }
+}
---
+++ lab2/HtmlRefValidityChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using HtmlRefValidityChecker.HtmlParser;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HtmlLinkParser htmlLinkParser = new HtmlLinkParser(new Uri("http://links.qatl.ru/"));
+ISet<string> links = new HashSet<string>();
+links = htmlLinkParser.GetAllPageLinks();
+foreach (var link in links)
+{
+    Console.WriteLine(link);
+}
