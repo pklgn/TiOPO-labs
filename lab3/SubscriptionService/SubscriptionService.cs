ec55767077e7c4983c78b3f6473331c01419c886
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-10-05 22:41:26 +0300

    [lab3] add SubscriptionService class implementation

--- lab3/SubscriptionService/SubscriptionService.cs
+++ lab3/SubscriptionService/SubscriptionService.cs
@@ -1,83 +1,113 @@
-﻿using System.Collections;
-using System.Data.SqlTypes;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 namespace SubscriptionService;
 
+using AdditionalOption = Tuple<Guid, string>;
+
 public interface IBenefitService
 {
-				IEnumerable<string> GetBenefitsForCategory(Guid categoryGuid);
-				DateTime GetBenefitExpireDateTime(Guid benefitGuid);
-				IEnumerable<string> GetBenefitsForSubscriptionPlan(Guid subscriptionPlanGuid);
+    IEnumerable<string> GetBenefitsForCategory(Guid categoryGuid);
+    DateTime GetBenefitExpireDateTime(Guid benefitGuid);
 }
 
 public interface IEmailSender
 {
-				public void SendEmail(string recipient, string subject, string body);
+    public void SendEmail(string recipient, string subject, string body);
 }
 
 public class SubscriptionService
 {
-				private IBenefitService _benefitService;
-				private IEmailSender _emailSender;
+    private IBenefitService _benefitService;
+    private IEmailSender _emailSender;
 
-				//ежемесячная базовая стоимость
-				private decimal _monthlyRate;
+    //ежемесячная базовая стоимость
+    private decimal _monthlyRate;
 
-				//дополнительные опции подписки
-				private List<Tuple<Guid, string>> _additionalOptions = new List<Tuple<Guid, string>>();
+    //дополнительные опции подписки
+    private List<AdditionalOption> _additionalOptions = new List<Tuple<Guid, string>>();
 
-				public SubscriptionService(IBenefitService benefitService, IEmailSender emailSender, decimal monthlyRate)
-				{
-								_benefitService = benefitService;
-								_emailSender = emailSender;
-								_monthlyRate = monthlyRate;
-				}
+    public SubscriptionService(IBenefitService benefitService, IEmailSender emailSender, decimal monthlyRate)
+    {
+        _benefitService = benefitService;
+        _emailSender = emailSender;
+        _monthlyRate = monthlyRate;
+    }
 
-				string GetBenefitFromCategory(string keyword, Guid categoryId)
-				{
-								var benefits = _benefitService.GetBenefitsForCategory(categoryId);
-								
-								var appropriateBenefit = benefits.Where(benefit => benefit.ToLower().Contains(keyword)).FirstOrDefault();
+    string GetBenefitFromCategory(string keyword, Guid categoryId)
+    {
+        var benefits = _benefitService.GetBenefitsForCategory(categoryId);
+        var appropriateBenefit = benefits.Where(benefit => benefit.ToLower().Contains(keyword)).FirstOrDefault();
 
-								if (appropriateBenefit == null)
-								{
-												return "Benefit for specifiend keyword wasn't foundn\n";
-								}
+        if (appropriateBenefit == null)
+        {
+            return "Benefit for specifiend keyword wasn't found\n";
+        }
 
-								return appropriateBenefit;
-				}
+        return appropriateBenefit;
+    }
 
-				DateTime GetBenefitExpireDateTime(Guid benefitGuid)
-				{
-								
-								return _benefitService.GetBenefitExpireDateTime(benefitGuid);
+    DateTime GetBenefitExpireDateTime(Guid benefitGuid)
+    {
+        return _benefitService.GetBenefitExpireDateTime(benefitGuid);
+    }
 
-				}
+    public void SendEmail(string recipient, string notification)
+    {
+        string subject = "Subscription service updates";
+        string body = $"Hi,\n{notification}.\nSincerely, the service team.";
 
-				IEnumerable<string> GetBenefitsForSubscriptionPlan(Guid subscriptionPlanGuid)
-				{
-				}
+        _emailSender.SendEmail(recipient, subject, body);
+    }
 
-				public void SendEmail(string recipient, string notification)
-				{
-								string subject = "Subscription service updates";
-								string body = $"Hi,\n{notification}.\nSincerely, the service team.";
+    public decimal CalculatePriceForPeriod(DateTime periodStart, DateTime periodEnd)
+    {
+        //TODO: логика того, что подписка не может быть меньше чем на месяц
+        if (periodStart > periodEnd)
+        {
+            throw new ArgumentException($"Start period {periodStart} must precede end period value {periodEnd}");
+        }
 
-								_emailSender.SendEmail(recipient, subject, body);
-				}
+        var periodInMonths = GetDiffInMonths(periodStart, periodEnd);
 
-				public decimal CalculatePriceForPeriod(DateTime periodStart, DateTime periodEnd)
-				{
-								//TODO: логика того, что подписка не может быть меньше чем на месяц
+        if (periodInMonths == 0)
+        {
+            throw new Exception("The difference between the periods cannot be less than one month");
+        }
 
-								return _monthlyRate;
-				}
+        return _monthlyRate * periodInMonths;
+    }
 
-				public Guid CreateAdditionOption(string name, string decription)
-				{
-				}
+    public Guid CreateAdditionOption(string name, string decription)
+    {
+        Guid guid = Guid.NewGuid();
+        string additionalOptionString = $"{name}: {decription}";
+        _additionalOptions.Add(new AdditionalOption(guid, additionalOptionString));
 
-				public void RemoveAdditionOption(Guid additionalOptionGuid)
-				{
-				}
+        return guid;
+    }
+
+    public void RemoveAdditionOption(Guid additionalOptionGuid)
+    {
+        _additionalOptions.Where(option => option.Item1 == additionalOptionGuid).FirstOrDefault();
+    }
+
+    public IEnumerable<AdditionalOption> GetAdditionalOptions()
+    {
+        return _additionalOptions;
+    }
+
+    private static Int32 GetDiffInMonths(DateTime start, DateTime end)
+    {
+        Int32 months = 0;
+        DateTime tmp = start;
+
+        while (tmp < end)
+        {
+            months++;
+            tmp = tmp.AddMonths(1);
+        }
+
+        return months;
+    }
 }
