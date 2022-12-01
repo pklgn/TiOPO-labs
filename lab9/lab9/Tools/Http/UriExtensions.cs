1986a26c821c1b9daf8e44efd31b5f07ac319253
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-12-01 13:17:02 +0300

    [lab9] add some tools

---
+++ lab9/lab9/Tools/Http/UriExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace lab9.Tools.Http;
+
+internal class UriExtension
+{
+    public static Uri AddParameter(Uri uri, string? paramName, string? paramValue)
+    {
+        if (paramName is null || paramValue is null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        var uriBuilder = new UriBuilder(uri);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        query[paramName] = paramValue;
+        uriBuilder.Query = query.ToString();
+
+        return uriBuilder.Uri;
+    }
+}
---
+++ lab9/lab9/Tools/String/Translit.cs
@@ -0,0 +1,75 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace lab9.Tools.String;
+
+internal class Translit
+{
+    static Dictionary<char, string> translation = new Dictionary<char, string>()
+    {
+        { 'а', "a" },
+        { 'б', "b" },
+        { 'в', "v" },
+        { 'г', "g" },
+        { 'д', "d" },
+        { 'e', "e" },
+        { 'ё', "yo" },
+        { 'ж', "g" },
+        { 'з', "z" },
+        { 'и', "i" },
+        { 'й', "y" },
+        { 'к', "k" },
+        { 'л', "l" },
+        { 'м', "m" },
+        { 'н', "n" },
+        { 'о', "o" },
+        { 'п', "p" },
+        { 'р', "r" },
+        { 'с', "s" },
+        { 'т', "t" },
+        { 'у', "u" },
+        { 'ф', "f" },
+        { 'х', "kc" },
+        { 'ц', "ts" },
+        { 'ч', "ch" },
+        { 'ш', "sh" },
+        { 'щ', "shsh" },
+        { 'ъ', "ie" },
+        { 'ы', "y" },
+        { 'ь', "'" },
+        { 'э', "e" },
+        { 'ю', "iu" },
+        { 'я', "ia" },
+        { ' ', "-" },
+        { '&', "-and-" },
+    };
+
+    string ToTranslit(char c)
+    {
+        string? result;
+
+        return translation.TryGetValue(c, out result)
+            ? result
+            : c.ToString();
+    }
+
+    string ToTranslit(string src)
+    {
+        string result = "";
+
+        foreach (char c in src)
+        {
+            result += ToTranslit(c);
+        }
+
+        return result;
+    }
+
+}
