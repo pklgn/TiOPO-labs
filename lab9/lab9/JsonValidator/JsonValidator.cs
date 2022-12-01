3de0ae9f13334c25f7eb7d09437e74494cc8b8b8
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-12-01 13:16:37 +0300

    [lab9] add simple json validator based on JToken

---
+++ lab9/lab9/JsonValidator/JsonValidator.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json;
+
+namespace lab9.JsonValidator;
+
+public class JsonValidator
+{
+    public bool wasSuccess { get; private set; } = false;
+
+    public void Validate(string schemaString, JToken json)
+    {
+        JSchema schema = JSchema.Parse(schemaString);
+
+        if (!json.IsValid(schema))
+        {
+            //TODO: add custom exception
+            wasSuccess = false;
+            throw new Exception("Object schema doesn't match target schema");
+        }
+
+        wasSuccess = true;
+    }
+}
