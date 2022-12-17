48b13484fe4489a69189297048e3a6c840657ea4
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-12-17 17:24:14 +0300

    [lab9] refactor items in json schema

--- lab9/lab9/JsonValidator/JsonValidator.cs
+++ lab9/lab9/JsonValidator/JsonValidator.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,13 +15,22 @@ using Newtonsoft.Json;
 
 namespace lab9.JsonValidator;
 
+
 public class JsonValidator
 {
     public bool wasSuccess { get; private set; } = false;
 
+
     public void Validate(string schemaString, JToken json)
     {
-        JSchema schema = JSchema.Parse(schemaString);
+        JSchemaUrlResolver resolver = new JSchemaUrlResolver();
+        var path = GetThisFilePath();
+        var directory = Directory.GetParent(path)!.Parent!;
+        JSchema schema = JSchema.Parse(schemaString, new JSchemaReaderSettings
+        {
+            Resolver = resolver,
+            BaseUri = new Uri(directory.ToString() + Path.DirectorySeparatorChar)
+        });
 
         if (!json.IsValid(schema))
         {
@@ -28,4 +39,10 @@ public class JsonValidator
 
         wasSuccess = true;
     }
+
+    private static string GetThisFilePath([CallerFilePath] string path = null)
+    {
+        return path;
+    }
+
 }
