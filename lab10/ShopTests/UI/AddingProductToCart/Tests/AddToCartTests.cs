b7a6424f9a3d97eeb867b793c2be052e55848be9
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-12-19 22:56:27 +0300

    [lab10] add tests for product cart

---
+++ lab10/ShopTests/UI/AddingProductToCart/Tests/AddToCartTests.cs
@@ -0,0 +1,128 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using ShopTests.UI.AddingProductToCart.WebDriverMethods;
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace ShopTests.UI.AddingProductToCart.Tests
+{
+    internal static class CustomCartAssert
+    {
+        public static void IsSameCartProduct(this Assert assert, CartProduct expected, CartProduct actual)
+        {
+            Assert.AreEqual(expected.name.ToLower(), actual.name.ToLower(),
+                "Expected to get same product names");
+            Assert.AreEqual(expected.quantity, string.IsNullOrEmpty(actual.quantity) ? "0" : actual.quantity,
+                "Expected to get same product quantities");
+            Assert.AreEqual(expected.price, actual.price,
+                "Expected to get same product prices");
+        }
+
+        public static void IsSameCartTotal(this Assert assert, CartTotal expected, CartTotal actual)
+        {
+            Assert.AreEqual(expected.quantity, string.IsNullOrEmpty(actual.quantity) ? "0" : actual.quantity,
+                "Expected to get same cart quantities");
+            Assert.AreEqual(expected.price, actual.price,
+                "Expected to get same cart prices");
+        }
+    }
+
+    [TestClass]
+    public class AddToCartTests
+    {
+        private static readonly int COMMON_PRODUCT_COUNT = 1;
+        private AddToCartMethods _addToCartMethods;
+        private IWebDriver _webDriver;
+        private string _url = "http://shop.qatl.ru/";
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver(Environment.GetEnvironmentVariable("CHROME_DRIVER_DIR"));
+            _webDriver.Navigate().GoToUrl(_url);
+            _webDriver.Manage().Window.Maximize();
+
+            _addToCartMethods = new AddToCartMethods(_webDriver);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _webDriver.Quit();
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    "..\\..\\UI\\AddingProductToCart\\Configs\\Cart.xml",
+                    "TestProductPage",
+                    DataAccessMethod.Sequential)]
+        public void AddToCartFromProductPage()
+        {
+            var productLink = TestContext.DataRow["Link"].ToString();
+            _webDriver.Navigate().GoToUrl($"{_url}{productLink}");
+
+            _addToCartMethods.SetBaseElement(_addToCartMethods.GetSimpleCartElement());
+            var quantity = TestContext.DataRow["Quantity"].ToString();
+            _addToCartMethods.AddProductToCart(int.Parse(quantity));
+
+            _addToCartMethods.SetBaseElement(_addToCartMethods.GetModalElement());
+
+            var cartProduct = _addToCartMethods.GetCartProduct(0);
+
+            var name = TestContext.DataRow["Name"].ToString();
+            var price = TestContext.DataRow["Price"].ToString();
+
+            Assert.That.IsSameCartProduct(new CartProduct(name, quantity, price), cartProduct);
+
+            var cartTotal = _addToCartMethods.GetCartTotal();
+            var totalQuantity = TestContext.DataRow["TotalQuantity"].ToString();
+            var totalPrice = TestContext.DataRow["TotalPrice"].ToString();
+            Assert.That.IsSameCartTotal(new CartTotal(totalQuantity, totalPrice), cartTotal);
+
+            Assert.AreEqual(totalPrice, _addToCartMethods.GetSimpleCartTotal());
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "..\\..\\UI\\AddingProductToCart\\Configs\\Cart.xml",
+            "TestProductList",
+            DataAccessMethod.Sequential)]
+        public void AddToCartFromProductList()
+        {
+            CartProduct firstProduct = new CartProduct(
+                TestContext.DataRow["FirstProductName"].ToString(),
+                COMMON_PRODUCT_COUNT.ToString(),
+                TestContext.DataRow["FirstProductPrice"].ToString()
+                );
+            var firstProductLink = TestContext.DataRow["FirstProductLink"].ToString();
+            _addToCartMethods.SetBaseElement(_addToCartMethods.GetProductItemCartElement(firstProductLink));
+            _addToCartMethods.AddProductToCart(COMMON_PRODUCT_COUNT);
+
+            _addToCartMethods.CloseModal();
+
+            CartProduct secondProduct = new CartProduct(
+                TestContext.DataRow["SecondProductName"].ToString(),
+                COMMON_PRODUCT_COUNT.ToString(),
+                TestContext.DataRow["SecondProductPrice"].ToString()
+                );
+            var secondProductLink = TestContext.DataRow["SecondProductLink"].ToString();
+            _addToCartMethods.SetBaseElement(_addToCartMethods.GetProductItemCartElement(secondProductLink));
+            _addToCartMethods.AddProductToCart(COMMON_PRODUCT_COUNT);
+
+            var firstCartProduct = _addToCartMethods.GetCartProduct(0);
+            var secondCartProduct = _addToCartMethods.GetCartProduct(1);
+
+            Assert.That.IsSameCartProduct(firstProduct, firstCartProduct);
+            Assert.That.IsSameCartProduct(secondProduct, secondCartProduct);
+
+            var cartTotal = _addToCartMethods.GetCartTotal();
+            var totalQuantity = TestContext.DataRow["TotalQuantity"].ToString();
+            var totalPrice = TestContext.DataRow["TotalPrice"].ToString();
+            Assert.That.IsSameCartTotal(new CartTotal(totalQuantity, totalPrice), cartTotal);
+        }
+    }
+}
---
+++ lab10/ShopTests/UI/AddingProductToCart/WebDriverMethods/AddToCartMethods.cs
@@ -0,0 +1,156 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ShopTests.UI.AddingProductToCart.WebDriverMethods
+{
+    struct CartProduct
+    {
+        public CartProduct(string name, string quantity, string price)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public string name;
+        public string quantity;
+        public string price;
+    }
+
+    struct CartTotal
+    {
+        public CartTotal(string quantity, string price)
+        {
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public string quantity;
+        public string price;
+    }
+
+    internal class AddToCartMethods
+    {
+        private IWebDriver _webDriver;
+        private IWebElement _baseElement;
+        private static readonly By
+            _cartAddXPath = By.XPath(".//*[contains(@class, 'add-to-cart-link')]"),
+            _cartQuantityXPath = By.XPath("//input[@name='quantity']"),
+            _cartXPath = By.XPath("//*[contains(@class,'simpleCart_shelfItem')]"),
+            _modalXPath = By.XPath("//div[contains(@class,'modal-content')]"),
+            _modalProductRowsXPath = By.XPath(".//tr[descendant::a]"),
+            _modalTotalXPath = By.XPath(".//tr[descendant::td[contains(@class, 'cart')]]"),
+            _cartSimpleTotalXpath = By.XPath("//*[@class='simpleCart_total']"),
+            _modalCloseXPath = By.XPath("//*[contains(@class, 'close')]");
+
+        private Dictionary<string, int> _modalProductRowColumns = new Dictionary<string, int>
+        {
+            {"product", 0},
+            {"name", 1},
+            {"quantity", 2},
+            {"price", 3},
+        };
+
+        private Dictionary<string, int> _modalTotal = new Dictionary<string, int>
+        {
+            {"quantity", 1},
+            {"price", 1},
+        };
+
+        public AddToCartMethods(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void SetBaseElement(IWebElement webElement)
+        {
+            _baseElement = webElement;
+        }
+
+        public IWebElement GetSimpleCartElement()
+        {
+            return _webDriver.FindElement(_cartXPath);
+        }
+
+        public void AddProductToCart(int quantity)
+        {
+            if (quantity != 1)
+            {
+                var quantityElement = _baseElement.FindElement(_cartQuantityXPath);
+                quantityElement.Clear();
+                quantityElement.SendKeys(quantity.ToString());
+            }
+
+            var cartAdd = _baseElement.FindElement(_cartAddXPath);
+            cartAdd.Click();
+
+            WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            wait.Until(e => _webDriver
+                .FindElement(_modalXPath)
+                .Displayed);
+        }
+
+        public IWebElement GetModalElement()
+        {
+            return _webDriver.FindElement(_modalXPath);
+        }
+
+        public ReadOnlyCollection<IWebElement> GetCartProducts()
+        {
+            return _webDriver.FindElements(_modalProductRowsXPath);
+        }
+
+        public CartProduct GetCartProduct(int index)
+        {
+            var products = _webDriver.FindElements(_modalProductRowsXPath);
+            var product = products[index];
+
+            var columns = product.FindElements(By.XPath(".//td"));
+
+            return new CartProduct(
+                columns[_modalProductRowColumns["name"]].Text,
+                columns[_modalProductRowColumns["quantity"]].Text,
+                columns[_modalProductRowColumns["price"]].Text
+                );
+        }
+
+        public CartTotal GetCartTotal()
+        {
+            var total = _webDriver.FindElements(_modalTotalXPath);
+
+            var totalQuantityRow = total.First();
+            var totalPriceRow = total.Last();
+
+            var totalQuantityColumns = totalQuantityRow.FindElements(By.XPath(".//td"));
+            var totalPriceColumns = totalPriceRow.FindElements(By.XPath(".//td"));
+
+            return new CartTotal(
+                totalQuantityColumns[_modalTotal["quantity"]].Text,
+                totalPriceColumns[_modalTotal["price"]].Text.Remove(0, 1)
+                ); ;
+        }
+
+        public string GetSimpleCartTotal()
+        {
+            return _webDriver.FindElement(_cartSimpleTotalXpath).Text.Remove(0, 1);
+        }
+
+        public IWebElement GetProductItemCartElement(string link)
+        {
+            return _webDriver.FindElement(By.XPath($"//div[descendant::a[contains(@href, '{link}')] and contains(@class, 'product-bottom')]"));
+        }
+
+        public void CloseModal()
+        {
+            _webDriver.FindElement(_modalCloseXPath).Click();
+        }
+    }
+}
